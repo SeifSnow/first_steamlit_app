7e388632984738572b8e1af0e37f74ebb9e3f9ee
SeifSnow <114738134+SeifSnow@users.noreply.github.com>
2022-09-30 13:41:58 +0200

    Update streamlit_app.py
    Add some recipes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('My Parent New Healthy Diner')
 
 streamlit.header('BreakFast Menu')
-streamlit.text('Omega 3 & blueberry outmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & blueberry outmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
