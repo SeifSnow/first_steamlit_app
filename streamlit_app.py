dfde869860dc9360e1e1715cf243e54c79c3e932
SeifSnow <114738134+SeifSnow@users.noreply.github.com>
2022-09-30 12:38:30 +0200

    Update streamlit_app.py
    Added recepie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parent New Healthy Diner')
+
+streamlit.header('BreakFast Menu')
+streamlit.text('Omega 3 & blueberry outmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
