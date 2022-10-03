ec2b41d3f3cc4344712e5c6453582036d8351dd7
SeifSnow <114738134+SeifSnow@users.noreply.github.com>
2022-10-03 13:30:25 +0200

    Update streamlit_app.py
    streamlit input added for fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,8 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+fruit_choice_add = streamlit.text_input('What fruit would you like to add?','jackfruit')
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + fruit_choice_add +"')")
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains : ")
